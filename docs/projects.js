9a68c5e07352b41ec1812d61d761e9fd563aa68a
dlamb95 <73131690+dlamb95@users.noreply.github.com>
2025-11-18 20:21:19 -0500

    Update projects.js

--- docs/projects.js
+++ docs/projects.js
@@ -1,11 +1,12 @@
 const projects = [
   {
-    name: "Renewable Energy Forecasting",
-    description:
-      "Predicted solar and wind energy production using EIA and CIMIS datasets through ARIMA/SARIMA and regression models to identify seasonal and temperature trends.",
-    link: "https://github.com/D-Lamb95/renewable-energy-forecasting",
-    skills: ["Python", "Pandas", "ARIMA/SARIMA", "Regression", "Matplotlib"],
-  },
+  name: "Renewable Energy Forecasting",
+  description:
+    "Predicted solar and wind energy production using EIA and CIMIS datasets through ARIMA/SARIMA and regression models to identify seasonal and temperature trends.",
+  page: "renewable-energy-forecasting.html",
+  link: "https://github.com/D-Lamb95/renewable-energy-forecasting",
+  skills: ["Python", "Pandas", "ARIMA/SARIMA", "Regression", "Matplotlib"],
+},
   {
     name: "Fraud Detection & AML Big Data Architecture",
     description:
